01acf5a086841ac604033bb4034824529985b666
CAS-AU-0707 <cs0959a@american.edu>
2024-10-22 21:34:16 -0400

    basic platformer #2

--- assignments/My project/Assets/Player.cs
+++ assignments/My project/Assets/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
@@ -16,6 +17,15 @@ public class Player : MonoBehaviour
     float moveSpeed = 15f;
     float turnSpeed = 90f;
 
+    int col = 3;
+    float timer = 0;
+    bool gameover = false;
+    bool ig = false;
+
+    public TMP_Text colletables;
+    public TMP_Text time;
+    public TMP_Text center;
+
 
 
     // Start is called before the first frame update
@@ -27,27 +37,68 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float hAxis = Input.GetAxis("Horizontal");
-        float vAxis = Input.GetAxis("Vertical");
-
-        transform.Rotate(0, turnSpeed * hAxis * Time.deltaTime, 0);
-        
-        Vector3 amountToMove = transform.forward * moveSpeed * vAxis;
-
-        if (!cc.isGrounded){
-            vel += grav * Time.deltaTime;
+        if (Input.GetKey(KeyCode.P)){
+            ig = true;
         }
-        else{
-            vel = -2.5f;
+
+        if (!gameover || ig){
+            float hAxis = Input.GetAxis("Horizontal");
+            float vAxis = Input.GetAxis("Vertical");
+
+            transform.Rotate(0, turnSpeed * hAxis * Time.deltaTime, 0);
+        
+            Vector3 amountToMove = transform.forward * moveSpeed * vAxis;
+
+            if (!cc.isGrounded){
+                vel += grav * Time.deltaTime;
+            }
+            else{
+                vel = -2.5f;
             
-            if(Input.GetKey(KeyCode.Space)){
-                vel = jump;
+                if(Input.GetKey(KeyCode.Space)){
+                    vel = jump;
+                }
+            }
+        
+            amountToMove.y += vel;
+            amountToMove *= Time.deltaTime;
+
+            cc.Move(amountToMove);
+
+            timer += Time.deltaTime;
+
+            if(col == 0){
+                    center.text = "You Win!";
+                    time.text = "";
+                colletables.text = "";
+            }
+            else if(timer >= 20){
+                gameover = true;
+                time.text = "";
+                colletables.text = "";
+            }
+            else{
+                time.text = ((int)(20f - timer)).ToString();
+                colletables.text = col.ToString();
+                center.text = "";
             }
         }
-        
-        amountToMove.y += vel;
-        amountToMove *= Time.deltaTime;
+        else{
+            center.text = "Game Over";
+        }
+    }
 
-        cc.Move(amountToMove);
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("collectable")){
+            
+            // Removes/destroys the Collectable
+            Destroy(other.gameObject);
+
+            // Adds to score and scoreText
+            col--;
+            timer = 0;
+
+        }     
     }
 }

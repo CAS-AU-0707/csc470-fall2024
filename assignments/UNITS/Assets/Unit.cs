fab7c7f94b0ebba07c560753947a0757c919835d
CAS-AU-0707 <cs0959a@american.edu>
2024-11-16 14:59:55 -0500

    emergance redone

--- assignments/UNITS/Assets/Unit.cs
+++ assignments/UNITS/Assets/Unit.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+float hover;
+bool peak = true;
+
 public class Unit : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        float rand = Random.Range(0f, 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        hover = Mathf.Sin((Time.deltaTime()* Mathf.PI) + rand);
+
+        transform.position = new Vector3(transform.position.x, hover, transform.position.z);
     }
 }
